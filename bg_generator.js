a99ff0689f4bc3449df9669bea0fb03d83b9b01e
Your Name <you@example.com>
2023-06-05 21:07:17 +0530

    backgroundgenerator
    Signed-off-by: Your Name <you@example.com>

---
+++ bg_generator.js
@@ -0,0 +1,28 @@
+var lodash = require('lodash');
+
+let array_of_numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+
+console.log(lodash.without(array_of_numbers, 3));
+
+var body = document.getElementById('gradient');
+// var input1 = document.getElementsByClassName("input1")[0];
+var input1 = document.querySelector(".input1")
+var input2 = document.getElementsByClassName("input2")[0];
+var h3 = document.getElementsByTagName('h3')[0];
+
+// console.log(input1);
+// console.log(input2);
+// console.log(h3);
+
+
+function set_gradient(event) {
+    // console.log(event.target.value);
+    // console.log(input1.value);
+    body.style.background = "linear-gradient(to right" + "," + input1.value + "," + input2.value + ")";
+
+    h3.textContent = body.style.background;
+}
+
+
+input1.addEventListener('input', set_gradient)
+input2.addEventListener('input', set_gradient)
